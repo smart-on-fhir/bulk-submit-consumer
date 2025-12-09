7a4c07a9051a9ec8fbcf46a33b21c2dd7927498c
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-12-09 08:01:20 -0500

    Add favicon

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,8 @@ export default function createApp() {
     app.use(cors());
     app.use(express.json({ limit: '1mb' }));
 
+    app.use(express.static(path.resolve(__dirname, '..', 'static')));
+ 
     // Set the view engine to Pug
     app.set('view engine', 'pug');
     app.set('views', path.join(__dirname, 'views'));
