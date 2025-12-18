3ee7230ebe74302ca915450dc56440978c2cebb5
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-12-18 08:42:08 -0500

    Recognize some FHIR types as JSON

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ export default function createApp() {
     app.disable('x-powered-by');
 
     app.use(cors());
-    app.use(express.json({ limit: '1mb' }));
+    app.use(express.json({ limit: '1mb', type: ['application/fhir+json', 'application/json+fhir', 'application/json'] }));
 
     app.use(express.static(path.resolve(__dirname, '..', 'static')));
  
